d38b0986eb0891fd440f39690b4cd98b5ad44240
Jermaine <jermerf@gmail.com>
2020-04-24 11:37:03 -0300

    And additional changes

--- ejs-sample/app.js
+++ ejs-sample/app.js
@@ -12,6 +12,13 @@ app.get("/", (req, res) => {
   res.render('home')
 })
 
+app.get("/login", (req, res) => {
+  res.render('login', {
+    isLoggedIn: false,
+    username: "Jermerf"
+  })
+})
+
 app.use("/public", express.static('public'))
 
 
